72e9322a22673959303045db19cf576cb8353ef8
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-09-21 01:03:08 +0530

    Add reactredux project as normal folder

---
+++ reactredux/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import PizzaBox from './components/Pizza/PizzaBox';
+function App() {
+  return (
+    <div className="App">
+      <PizzaBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactredux/src/components/Pizza/PizzaBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Pizza/Pizzabox.css"
+
+const PizzaBox = () => {
+  return (
+    <div className='main-div'>
+      <h1>No of pizzabases available:100</h1>
+      <button >Order Pizza</button>
+    </div>
+  )
+}
+
+export default PizzaBox
