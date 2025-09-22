c6f50db4c47d1d96139fb456761d7733e79cfa9c
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-09-23 01:28:46 +0530

    added comments

--- reactredux/src/components/redux/HooksContainer.js
+++ reactredux/src/components/redux/HooksContainer.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import "../PizzaBase/Pizzabox.css"
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux' 
+// useSelector ~ replaces mapStateToProps
+// useDispatch ~ replaces mapDispatchToProps
+
 import orderPizza from './pizza/PizzaActions'
 
-
-
-//pizzabox react component
+// pizzabox react component
 const HooksContainer = () => {
 
-    const pizzaBase=useSelector(state=>state.pizzaBase)
-    const dispatch = useDispatch()
-  return (
-    <div className='main-div'>
-      <h1>No of pizzabases available:{pizzaBase}</h1>
-      <button onClick={()=>dispatch(orderPizza())}  >Order Pizza</button>
-    </div>
-  )
-}
+    // useSelector pulls data directly from Redux store state
+    // same as mapStateToProps(state) => { pizzaBase: state.pizzaBase }
+    const pizzaBase = useSelector(state => state.pizzaBase)
 
+    //useDispatch gives access to dispatch function
+    // same as mapDispatchToProps(dispatch) => { orderPizza: () => dispatch(orderPizza()) }
+    const dispatch = useDispatch()
+
+    return (
+        <div className='main-div'>
+            <h1>No of pizzabases available: {pizzaBase}</h1>
+            
+            {/*     dispatching an action with useDispatch
+                 same as props.orderPizza() when using connect */}
+            <button onClick={() => dispatch(orderPizza())}>
+                Order Pizza
+            </button>
+        </div>
+    )
+}
 
 export default HooksContainer
