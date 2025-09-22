7e88aaae6c1eedaabc159b50d3203e154bc2c7c9
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-09-23 02:21:43 +0530

    chore:implemented logger middleware

--- reactredux/src/components/redux/store.js
+++ reactredux/src/components/redux/store.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware} from "redux"; //ported applyMiddleWareim
 import rootReducer from "./rootReducers";
+import logger from "redux-logger" // imported logger middleWare
+
 
 // create store using pizzaReducer (used createStore for study purpose only)
-const store = createStore(rootReducer);
+// passed applyMiddleware as second arguement to createstore
+const store = createStore(rootReducer,applyMiddleware(logger)); //passed logger as arguement to applyMiddleWare
 
 export default store; // make store available to the app
